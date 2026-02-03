131ec472e88059b2db7290d33017fa6ad70f5696
nawrasabuhalaweh <abuhalawehnawras@gmail.com>
2026-02-03 09:34:25 +0400

    Update and rename scara_kinematics.txt to scara_kinematics.py

--- scara_kinematics.py
+++ scara_kinematics.py
@@ -1,8 +1,7 @@
 # this file is used to calculate the appropriate number of steps
-# that the motors should rotate to
+# that the motors should rotate for every given position
 
 import math
-
 
 def getAbsStepsX(x,y,l1,l2):
     alpha = math.atan2(y,x)
@@ -10,7 +9,7 @@ def getAbsStepsX(x,y,l1,l2):
     denominator = 2 * math.sqrt(x**2 + y**2) * l1
     theta = alpha - math.acos(numerator/denominator)
     thetaDeg = (180 / math.pi) * theta
-    stepsTheta = 0.75 * thetaDeg
+    stepsTheta = 0.75 * thetaDeg # 0.75 steps / degree
     print(thetaDeg)
     return stepsTheta
 
@@ -19,7 +18,7 @@ def getAbsStepsY(x,y,l1,l2):
     denominator = 2 * l1 * l2
     phi = math.pi - math.acos(numerator/denominator)
     phiDeg = (180 / math.pi) * phi
-    stepsPhi = 0.75 * phiDeg
+    stepsPhi = 0.75 * phiDeg # 0.75 steps / degree
     print(phiDeg)
     return stepsPhi
 
@@ -29,6 +28,7 @@ def getRelSteps(x, y, prevTheta, prevPhi, l1, l2):
     stepsX = newTheta - prevTheta
     stepsY = newPhi - prevPhi
     return [stepsX, stepsY]
+
 
 
 
