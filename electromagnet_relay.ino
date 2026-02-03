f027ccdfc4b6eeb2433571932c07cf503f9ab07c
nawrasabuhalaweh <abuhalawehnawras@gmail.com>
2026-02-03 09:32:55 +0400

    Update and rename electromagnet relay.txt to electromagnet_relay.ino
    updated file to .ino and adjusted for LED

--- electromagnet_relay.ino
+++ electromagnet_relay.ino
@@ -1,10 +1,18 @@
+// this file contains the basis of the communication between python and the arduino.
+// it tells the arduino how to interpret commands from the serial monitor to turn it on and off.
+
 const int relayPin = 7;
+const int ledPin = 5;
+
 char receivedChar;
 boolean newData = false;
 
 void setup() {
   pinMode(relayPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
   digitalWrite(relayPin, LOW);
+  
   Serial.begin(9600);
   Serial.println("Ready for input");
 }
@@ -23,7 +31,10 @@ void recvOneChar() {
 void closeSwitch() {
   if (receivedChar == 't') {
     digitalWrite(relayPin, HIGH);
+    digitalWrite(ledPin, HIGH);
   } else if (receivedChar == 'f') {
+    digitalWrite(ledPin, LOW);
     digitalWrite(relayPin, LOW);
   }
+
 }
