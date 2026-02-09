0bbff530ac608875dfeba0130dd6c9eadcfb04ee
nawrasabuhalaweh <abuhalawehnawras@gmail.com>
2026-02-09 11:53:19 +0400

    Update scara_kinematics.py
    final version . dont remember what i did to be honest

--- scara_kinematics.py
+++ scara_kinematics.py
@@ -1,5 +1,5 @@
 # this file is used to calculate the appropriate number of steps
-# that the motors should rotate for every given position
+# that the motors should rotate to
 
 import math
 
@@ -8,9 +8,8 @@ def getAbsStepsX(x,y,l1,l2):
     numerator = x**2 + y**2 + l1**2 - l2**2
     denominator = 2 * math.sqrt(x**2 + y**2) * l1
     theta = alpha - math.acos(numerator/denominator)
-    thetaDeg = (180 / math.pi) * theta
-    stepsTheta = 0.75 * thetaDeg # 0.75 steps / degree
-    print(thetaDeg)
+    thetaDeg = (180 / math.pi) * theta - 90
+    stepsTheta = 0.75 * thetaDeg
     return stepsTheta
 
 def getAbsStepsY(x,y,l1,l2):
@@ -18,8 +17,7 @@ def getAbsStepsY(x,y,l1,l2):
     denominator = 2 * l1 * l2
     phi = math.pi - math.acos(numerator/denominator)
     phiDeg = (180 / math.pi) * phi
-    stepsPhi = 0.75 * phiDeg # 0.75 steps / degree
-    print(phiDeg)
+    stepsPhi = 0.75 * phiDeg
     return stepsPhi
 
 def getRelSteps(x, y, prevTheta, prevPhi, l1, l2):
@@ -28,7 +26,6 @@ def getRelSteps(x, y, prevTheta, prevPhi, l1, l2):
     stepsX = newTheta - prevTheta
     stepsY = newPhi - prevPhi
     return [stepsX, stepsY]
-
 
 
 
